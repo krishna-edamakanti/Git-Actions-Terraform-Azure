d4f0b3006910a1717b7aaaaec5dbd2b1466a50be
krishna-edamakanti <95684277+krishna-edamakanti@users.noreply.github.com>
2022-01-10 22:56:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,13 @@
 terraform {
-
-  required_version = ">=0.12"
-  
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "TerraformDemo"
+    storage_account_name = "terraformcodeops"
+    container_name       = "tfstatefile"
+    key                  = "krish.fiserv"
   }
 }
+module "RG" {
+  source   = "./modules/RG" #A
+  rgname   = var.rgname     #B
+  location = var.location
 
-provider "azurerm" {
-  features {}
-}
-
-resource "random_pet" "rg-name" {
-  prefix    = var.resource_group_name_prefix
-}
-
-resource "azurerm_resource_group" "rg" {
-  name      = random_pet.rg-name.id
-  location  = var.resource_group_location
-}
