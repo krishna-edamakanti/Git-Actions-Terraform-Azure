38130dae5c0025770eebcee1709531e42cb0d97a
krishna-edamakanti <95684277+krishna-edamakanti@users.noreply.github.com>
2022-01-10 23:08:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,4 +10,9 @@ module "RG" {
   source   = "./modules/RG" #A
   rgname   = var.rgname     #B
   location = var.location
-
+}
+module "SA" {
+  source   = "./modules/StorageAccount"
+  sname    = var.sname
+  rgname   = var.rgname
+  location = var.location
